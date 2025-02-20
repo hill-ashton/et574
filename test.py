28a5e02aac13e8e8ac2491415c0bdaacd88dde51
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-02-20 17:54:56 +0000

    changed sep, added notes

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 #Project Demo
 
 print("Hello World")
+#creating variables
 a,b,c="Hello","Python","World"
 print('0123456789'*5)
+#using sep
 print(a,"\t",b,"\t",c,sep="")
-print(a,b,c,sep='\t')
+print(a,b,c,sep='\n')

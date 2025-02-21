4061b0b1769a3fc256cf951412bc4da5e5fc1a39
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-02-21 00:36:52 +0000

    Added personalized greeting feature

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 #Ashton Hill
 #hello.py
+try:
+    courseNum = eval(input("Enter your course number: "))
+    print(f"Hello, welcome to the course ET{courseNum}")
 
-print("Hello World!")
+except:
+    print("Hello, world!")

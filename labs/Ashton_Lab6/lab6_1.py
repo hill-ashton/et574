8d9620993feea46ad6a9a31f3b8bfb782b4a45ce
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-03-18 22:30:08 +0000

    Create files + lab6_1 complete

---
+++ labs/Ashton_Lab6/lab6_1.py
@@ -0,0 +1,15 @@
+age = int(input("Please enter your age: "))
+if (age<0):
+    print("invalid age.")
+elif (age<2):
+    print("You're a baby.")
+elif (age<4):
+    print("You're a toddler.")
+elif (age<13):
+    print("You're a kid.")
+elif (age<20):
+    print("You're a teenager.")
+elif (age<65):
+    print("You're an adult.")
+else:
+    print("You're an elder.")

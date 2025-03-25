aadca66f609b7bebb020af602bf3442b03e3990f
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-03-25 16:54:06 +0000

    lab7_1-2 Complete

--- labs/Ashton_Lab7/lab7_1.py
+++ labs/Ashton_Lab7/lab7_1.py
@@ -1,2 +1,10 @@
 #lab7_1
 #Ashton Hill
+try:
+    i = int(input("Enter an integer number, and i'll tell you if its a multiple of ten: "))
+    if(i%10==0):
+        print(f"{i} is a multiple of ten.")
+    else:
+        print(f"{i} is not a multiple of ten.")
+except(ValueError):
+    print("Invalid input.")
---
+++ labs/Ashton_Lab7/lab7_2.py
@@ -0,0 +1,11 @@
+#lab7_2
+#Ashton Hill
+for i in range(3,1000):
+    if(i%3==0 and i%2==0):
+        print(f"{i}",end=" ")
+
+num = 1
+while num <997:
+    if(num%3==0 and num%2==0):
+        print(f"{num}",end=" ")
+    num+=1

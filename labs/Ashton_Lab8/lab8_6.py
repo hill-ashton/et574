6a802e035aa805ee9bab8b7836a133059db9f920
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-04-02 21:32:49 +0000

    Completed 8_6

--- labs/Ashton_Lab8/lab8_6.py
+++ labs/Ashton_Lab8/lab8_6.py
@@ -1,2 +1,22 @@
 #lab8_6
 #Ashton Hill
+def middle(l):
+    n = []
+    if len(l)>1:
+        l.pop(0)
+        l.pop(-1)
+        for i in l:
+            n.append(i)
+        return n
+    else:
+        return l
+#print(middle([1,2,3,4]))
+def main():
+    import random
+    n = random.randint(1,10)
+    numList = [i for i in range(1,n)]
+    if(len(numList)==1):
+        print("No change made to the list.")
+    print(f"List length = {n-1}\n{numList}\n{middle(numList)}")
+
+main()

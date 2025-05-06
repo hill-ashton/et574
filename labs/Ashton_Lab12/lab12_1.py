f2d636db45f0bfc5b9f52ca82d678519691085f8
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-05-06 23:14:47 +0000

    12_1 Completed

--- labs/Ashton_Lab12/lab12_1.py
+++ labs/Ashton_Lab12/lab12_1.py
@@ -1,9 +1,18 @@
 #lab12_1
 #ashton hill
 
-filename = 'Presidents.txt'
-with open(filename) as txt:
-    lines = txt.readlines()
+def displayFileLoop(path):
+    with open(path) as txt:
+        for line in txt:
+            print(line.rstrip())
 
-for line in lines:
-    print(line.rstrip())
+def displayFileList(path):
+    with open(path) as txt:
+        lines = txt.readlines()
+    for line in lines:
+        print(line.rstrip())
+
+
+path = '/workspaces/et574/labs/Ashton_Lab12/Presidents.txt'
+displayFileLoop(path)
+displayFileList(path)

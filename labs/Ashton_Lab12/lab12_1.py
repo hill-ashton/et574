eedebad204fa889ec445f9a0cf2896709a467371
hill-ashton <157679596+hill-ashton@users.noreply.github.com>
2025-05-07 18:54:20 +0000

    lab12 edit

--- labs/Ashton_Lab12/lab12_1.py
+++ labs/Ashton_Lab12/lab12_1.py
@@ -2,11 +2,13 @@
 #ashton hill
 
 def displayFileLoop(path):
+    print("Using Loop:")
     with open(path) as txt:
         for line in txt:
             print(line.rstrip())
 
 def displayFileList(path):
+    print("Using List:")
     with open(path) as txt:
         lines = txt.readlines()
     for line in lines:
@@ -15,4 +17,5 @@ def displayFileList(path):
 
 path = '/workspaces/et574/labs/Ashton_Lab12/Presidents.txt'
 displayFileLoop(path)
+print()
 displayFileList(path)
